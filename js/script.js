892068d2c16a8e41454c627da6a5ab3609d83025
Inesa Hangan <hanganines2143@gmail.com>
2025-12-11 16:17:42 +0200

    Adding the rest section and footer

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 const catalogWrapper = document.querySelector('[data-catalog]');
+let isActive = false,
+    i = 2;
 
-for(let i = 0; i < 3; i++){
-    const price = products[i].price.toLocaleString("ru-RU");
-    const modif = i == 2 ? " card_accent" : "";
-    productHTML = `<div class="card${modif}" id="${products[i].id}">
+products.forEach((product, index) => {
+
+    //logic add class .card_accent
+    if(i === 4 && !isActive){
+        isActive = !isActive;
+        i = 0;
+        console.log('yes');
+    } else if(i == 2 && isActive){
+        isActive = !isActive;
+        i = 0;
+        console.log('none');
+    }
+    ++i;
+
+    console.log(product);
+    const price = product.price.toLocaleString("ru-RU");
+    const modif = isActive ? " card_accent" : "";
+    productHTML = `<div class="card${modif}" id="${product.id}">
             <div class="card__wrapper-img">
-                <img src="./img/catalog/${products[i].imgSrc}" alt="Product velo 1" class="card__img">
+                <img src="./img/catalog/${product.imgSrc}" alt="Product velo 1" class="card__img">
             </div>
             <div class="card__body">
                 <div class="card__desc">
-                    <h3 class="card__title">${products[i].name}</h3>
+                    <h3 class="card__title">${product.name}</h3>
                     <span class="card__price">${price} руб</span>
                 </div>
                 <button type="button" class="card__button button">Купить</button>
@@ -18,5 +34,5 @@ for(let i = 0; i < 3; i++){
 
 
     catalogWrapper.insertAdjacentHTML('beforeend', productHTML);
-}
-
+    
+});
